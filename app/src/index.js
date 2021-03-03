257ac7b69f42cee38173d308f0f76ab82004385e
mikitapapok <popok11014@gmail.com>
2021-03-04 01:11:36 +0300

    fix remarks

--- app/src/index.js
+++ app/src/index.js
@@ -1,20 +1,24 @@
 import React,{useState} from 'react'
 import {render} from 'react-dom'
-import Exchange from './components/exchange-block'
+import ValueBlock from './components/exchange-block'
 const root=document.getElementById('root');
 
 
-function FinalBlock (){   
-        
+function CurrencyBlock (){   
+    const currentByn=2.61  
+    const currentUsd=0.81      
       const   [param,setParam]=useState({byn:'', usd:''})
+      const conevtToUsd=(event)=>setParam({byn: event.target.value, usd: +event.target.value*currentUsd})
+      const convertToByn=(event)=>setParam({byn: +event.target.value*currentByn, usd: event.target.value})
+      const clearValue=(event)=>setParam({byn: event.target.value='', usd: event.target.value=''})
     return(
             <div>
                 <h1>Live currency converter</h1>
-                <Exchange id="byn"  conversion={(event)=>setParam({byn: event.target.value, usd: +event.target.value*0.81})}  clear={(event)=>setParam({byn: event.target.value='', usd: event.target.value=''})} value={param.byn} currency="BYN:"></Exchange>
-                <Exchange id="usd"  conversion={(event)=>setParam({byn: +event.target.value*2.61, usd: event.target.value})} clear={(event)=>setParam({usd: event.target.value='', byn: event.target.value=''})}  value={param.usd} currency="$, USD:"></Exchange>
+                <ValueBlock id="byn"  conversion={conevtToUsd}  clearValue={clearValue} value={param.byn} currency="BYN:"></ValueBlock>
+                <ValueBlock id="usd"  conversion={convertToByn} clearValue={clearValue}  value={param.usd} currency="$, USD:"></ValueBlock>
             </div>
          )
     }
 
 
-render (<FinalBlock/>,root)
+render (<CurrencyBlock/>,root)
